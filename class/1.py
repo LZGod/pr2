fa6d13db5cd2107a7f955c8c3f5b058e846060f3
LZGod <31654760+LZGod@users.noreply.github.com>
2019-04-05 17:50:31 +0300

    Add files via upload

--- class/1.py
+++ class/1.py
@@ -1,27 +1,9 @@
-import random
-letters = 'abcdefghijklmnopqrstuvwxyz'
-alp = [i for i in letters]
-letter = random.choice(alp)
-ind = alp.index(letter) + 1
-while True:
-    print("Букву мне. Английскую. Пожалуйста.")
-    a = input()
-    if a in alp:
-        if a == letter:
-            print("Успех.")
-            break
-        elif a != letter:
-            index = alp.index(a) + 1
-            if index < ind:
-                print("Правее.")
-                continue
-            else:
-                print("Левее.")
-                continue
-    else:
-        print("Миссия провалена.")
-        continue
-
-
-
-
+import urllib.request
+#req = urllib.request.Request('https://api.vk.com/method/wall.get?owner_id=1&count=19&v=5.74&access_token=8423c2448423c2448423c244d08441f2a1884238423c244dee1644d9e90529494134bf8&offset=2')
+req = urllib.request.Request('https://api.vk.com/method/wall.getComments?owner_id=1&post_id=2442097&count=2&v=5.74&access_token=8423c2448423c2448423c244d08441f2a1884238423c244dee1644d9e90529494134bf8')
+response = urllib.request.urlopen(req)
+result = response.read().decode('utf-8')
+print(result)
+import json
+data = json.loads(result)
+#print(data['response']['items'][]['text'])
